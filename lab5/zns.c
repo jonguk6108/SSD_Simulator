10bf797561c1c306ac454a5b8f59820e39ff6ed6
PARK JONGUK <jonguk6108@gmail.com>
2021-10-31 15:47:54 +0900

    third commit

--- lab5/zns.c
+++ lab5/zns.c
@@ -25,7 +25,7 @@ int MAX_OPEN_ZONE;
 
 int NUM_FCG;
 /********** do not touch ***********/
-int** ZONE; // [zone number][state, write pointer, state lba];
+int** ZONE; // [zone number][state, write pointer, state lba]; // 0 empty, 1 open, 2 full, 3 tlopen
 u32** BUFFER; // [zone number][value of sectors + lba];
 int* ZTF; //[zone number] = blk number;
 int* FB; //[queue] = size(numbers of freeblk), freeblk;
@@ -58,17 +58,16 @@ void zns_init(int nbank, int nblk, int npage, int dzone, int max_open_zone)
  
   for(int i = 0; i < max_open_zone; i++) {
     ZONE[i][0] = 0;
-    ZONE[i][1] = DEG_ZONE * NSECT * NPAGE;
-    ZONE[i][2] = DEG_ZONE * NSECT * NPAGE;
+    ZONE[i][1] = i * DEG_ZONE * NSECT * NPAGE;
+    ZONE[i][2] = i * DEG_ZONE * NSECT * NPAGE;
   }
   for(int i = 0; i < max_open_zone; i++)
     for(int j = 0; j < NSECT; j++)
       BUFFER[i][j] = - 1;
   for(int i = 0; i < max_open_zone; i++)
     ZTF[i] = -1;
-  for(int i = 0; i < NBLK; i++)
+  for(int i = 0; i <= NBLK; i++)
     FB[i] = i;
-  FB[NBLK] = 0;
   for(int i = 0; i < max_open_zone; i++)
     for(int j = 0; j < DEG_ZONE * NSECT * NPAGE; j++)
       TL_BITMAP[i][j] = 0;
@@ -89,17 +88,18 @@ int zns_write(int start_lba, int nsect, u32 *data)
     lba = lba / NPAGE;
     int c_fcg = lba % NUM_FCG;
 
-    int c_zone = lba / NUM_FCG;
+    int c_zone = lba;
     int c_bank = c_fcg * DEG_ZONE + b_offset;
 
     if(ZONE[c_zone][0] == 0 || ZONE[c_zone][0] == 1) {
+
       if(ZONE[c_zone][1] != c_lba)
         return -1;
 
       if(ZONE[c_zone][0] == 0) {
+        if(FB[NBLK] == NBLK - MAX_OPEN_ZONE) return -1;
         ZTF[c_zone] = FB[0];
 
-        if(FB[NBLK] == MAX_OPEN_ZONE) return -1;
         for(int i = 0; i < NBLK-1; i++)
           FB[i] = FB[i+1];
         FB[NBLK-1] = -1;
@@ -114,19 +114,79 @@ int zns_write(int start_lba, int nsect, u32 *data)
         u32 spare[] = {c_lba};
         nand_write(c_bank, ZTF[c_zone], p_offset, BUFFER[c_zone], spare);
       }
+
+      if(ZONE[c_zone][1] == ZONE[c_zone][2] + DEG_ZONE * NSECT * NPAGE) //full checking
+        ZONE[c_zone][0] = 2;
     }
 
     i_sect++;
   }
+  return 0;
 }
 
 void zns_read(int start_lba, int nsect, u32 *data)
 {
+  int i_sect = 0;
+  while(i_sect < nsect) {
+    int c_lba = start_lba + i_sect;
+    int lba = start_lba + i_sect;
+    int c_sect = lba % NSECT;
+    lba = lba / NSECT;
+    int b_offset = lba % DEG_ZONE;
+    lba = lba / DEG_ZONE;
+    int p_offset = lba % NPAGE;
+    lba = lba / NPAGE;
+    int c_fcg = lba % NUM_FCG;
+
+    int c_zone = lba;
+    int c_bank = c_fcg * DEG_ZONE + b_offset;
+
+    if(ZONE[c_zone][0] == 0)
+      return;
+    if(ZONE[c_zone][0] == 1) {
+      if(ZONE[c_zone][1] <= c_lba) {
+        data[i_sect] = -1;
+        i_sect++;
+        continue;
+      }
+
+      if( (ZONE[c_zone][1] / NSECT) * NSECT <= c_lba && c_sect != NSECT  )
+       data[i_sect] = BUFFER[ c_zone ][c_sect];
+      else {
+        u32 r_data[NSECT];
+        u32 spare[1];
+        nand_read(c_bank, ZTF[c_zone], p_offset, r_data, spare);
+        data[i_sect] = r_data[c_sect];
+      }
+    }
+
+    i_sect++;
+  }
   return;
 }
 
 int zns_reset(int lba)
 {
+  lba = lba / NSECT;
+  int b_offset = lba % DEG_ZONE;
+  lba = lba / DEG_ZONE;
+  lba = lba / NPAGE;
+  int c_fcg = lba % NUM_FCG;
+
+  int c_zone = lba;
+  int c_bank = c_fcg * DEG_ZONE + b_offset;
+
+  if(ZONE[c_zone][0] != 2)
+    return -1;
+
+  ZONE[c_zone][0] = 0;
+  ZONE[c_zone][1] = ZONE[c_zone][2];
+  nand_erase(c_bank, ZTF[c_zone]);
+  
+  FB[ FB[NBLK] ] = ZTF[c_zone];
+  FB[NBLK] += 1;
+  ZTF[c_zone] = -1;
+
   return 0;
 }
 
